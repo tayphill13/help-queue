a2a5e1a432919c695739105a2ef54b2905d68ff5
Taylor Phillips <taylorphillips133@gmail.com>
2020-09-08 22:09:34 -0700

    add change state functionality to allow for multiple tickets to queue

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,30 +1,14 @@
 import React from "react";
 import Ticket from "./Ticket";
-
-const masterTicketList = [
-  {
-    names: 'Thato and Haley',
-    location: '3A',
-    issue: 'Firebase won\'t save record. Halp.'
-  },
-  {
-    names: 'Sleater and Kinney',
-    location: '4B',
-    issue: 'Prop types are throwing an error.'
-  },
-  {
-    names: 'Imani & Jacob',
-    location: '9F',
-    issue: 'Child component isn\'t rendering.'
-  }
-];
+import PropTypes from 'prop-types'
 
 
-function TicketList() {
+
+function TicketList(props) {
   return (
     <React.Fragment>
       <hr/>
-      {masterTicketList.map((ticket, index) =>  
+      {props.ticketList.map((ticket, index) =>  
         <Ticket names={ticket.names}
         location={ticket.location}
         issue={ticket.issue}
@@ -33,5 +17,10 @@ function TicketList() {
     </React.Fragment>
   );
 }
+// Add propTypes for ticketList.
+TicketList.propTypes = {
+  ticketList: PropTypes.array
+};
+
 
 export default TicketList;
